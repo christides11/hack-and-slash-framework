82de374f2670b6145e52db45dd79937ab515e5e4
christides11 <christides11@hotmail.com>
2020-05-20 21:47:27 -0700

    Work on moveset handling.

---
+++ Assets/CAF/Combat/Moveset/MovesetDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace CAF.Combat
+{
+    [CreateAssetMenu(fileName = "MovesetDefinition", menuName = "CAF/Combat/Moveset")]
+    public class MovesetDefinition : NodeGraph
+    {
+        [Header("Normal Attacks")]
+        public List<MovesetAttackNode> groundAttackCommandNormals = new List<MovesetAttackNode>();
+        public List<MovesetAttackNode> groundAttackStartNodes = new List<MovesetAttackNode>();
+        public List<MovesetAttackNode> airAttackCommandNormals = new List<MovesetAttackNode>();
+        public List<MovesetAttackNode> airAttackStartNodes = new List<MovesetAttackNode>();
+    }
+}
---
+++ Assets/CAF/Input/InputDefinition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CAF.Input
+{
+    [System.Serializable]
+    public class InputDefinition
+    {
+        public int buttonID;
+
+        //Stick
+        public Vector2 stickDirection;
+        public float directionDeviation = 1.0f; // Directions are compared using dot product.
+    }
+}
