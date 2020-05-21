be035241773749810275617578b7c2c190b95eb5
christides11 <christides11@hotmail.com>
2020-05-20 20:34:38 -0700

    Combat stuff.

---
+++ Other/HelperFunctions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CAF
+{
+    public static class HelperFunctions
+    {
+        public static Vector2Int GetStickDir(Vector2 stick, float zone)
+        {
+            Vector2Int o = new Vector2Int(Mathf.Abs(stick.x) > zone ? signum(stick.x) : 0,
+                Mathf.Abs(stick.y) > zone ? signum(stick.y) : 0);
+            return o;
+        }
+
+        public static int signum(float v)
+        {
+            return v > 0 ? 1 : -1;
+        }
+    }
+}
