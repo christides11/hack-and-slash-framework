382f76caff03e58f7658d2d61cf8d50df21024f3
christides11 <christides11@hotmail.com>
2020-05-21 19:33:22 -0700

    Start of attack editor.

---
+++ Editor/Combat/Attack/AttackDefinitionEditorWindow.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CAF.Combat
+{
+    public class AttackDefinitionEditorWindow : EditorWindow
+    {
+        private AttackDefinition attack;
+        private int currentMenu = 0;
+
+        public static void Init(AttackDefinition attack)
+        {
+            AttackDefinitionEditorWindow window =
+                (AttackDefinitionEditorWindow)EditorWindow.GetWindow(typeof(AttackDefinitionEditorWindow));
+            window.attack = attack;
+            window.Show();
+        }
+
+        protected virtual void OnGUI()
+        {
+            attack = (AttackDefinition)EditorGUILayout.ObjectField("Attack", attack, typeof(AttackDefinition), false);
+
+            DrawMenuBar();
+            DrawMenu();
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(attack);
+            }
+        }
+
+        protected virtual void DrawMenuBar()
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("General"))
+            {
+                currentMenu = 0;
+            }
+            if (GUILayout.Button("Cancels"))
+            {
+                currentMenu = 1;
+            }
+            if (GUILayout.Button("Events"))
+            {
+                currentMenu = 2;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        protected virtual void DrawMenu()
+        {
+            if(currentMenu == 0)
+            {
+                DrawGeneralMenu();
+            }
+            if (currentMenu == 1)
+            {
+                DrawCancelWindows();
+            }
+            if(currentMenu == 2)
+            {
+                DrawEventsWindow();
+            }
+        }
+
+        protected virtual void DrawGeneralMenu()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            string attackName = EditorGUILayout.TextField("Name", attack.attackName);
+            EditorGUILayout.LabelField("Description");
+            string description = EditorGUILayout.TextArea(attack.description, GUILayout.Height(50));
+
+            int stateOverride = EditorGUILayout.IntField("State Override", attack.stateOverride);
+            int length = EditorGUILayout.IntField("Length", attack.length);
+
+            GUILayout.Space(15);
+
+            AnimationClip animationGround = (AnimationClip)EditorGUILayout.ObjectField("Animation (Ground)", attack.animationGround,
+                    typeof(AnimationClip), false);
+            AnimationClip animationAir = (AnimationClip)EditorGUILayout.ObjectField("Animation (Air)", attack.animationAir,
+                    typeof(AnimationClip), false);
+            WrapMode wrapMode = (WrapMode)EditorGUILayout.EnumPopup("Wrap Mode", attack.wrapMode);
+
+            GUILayout.Space(15);
+
+            float gravityScale = EditorGUILayout.FloatField("Gravity Scale Added", attack.gravityScaleAdded);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(attack, "Changed General Property.");
+                attack.attackName = attackName;
+                attack.description = description;
+                attack.stateOverride = stateOverride;
+                attack.length = length;
+                attack.animationGround = animationGround;
+                attack.animationAir = animationAir;
+                attack.wrapMode = wrapMode;
+                attack.gravityScaleAdded = gravityScale;
+            }
+        }
+
+        protected virtual void DrawCancelWindows()
+        {
+
+        }
+
+        protected virtual void DrawEventsWindow()
+        {
+
+        }
+    }
+}
