ccfef99cea5296c4e6f55b85229fe9af8b8d02fc
christides11 <christides11@hotmail.com>
2020-06-08 23:22:03 -0700

    Status effect support.

---
+++ Assets/CAF/Combat/Status Effects/StatusEffect.cs
@@ -0,0 +1,39 @@
+﻿using CAF.Entities;
+
+namespace CAF.Combat
+{
+    public class StatusEffect
+    {
+        protected int time;
+
+        public virtual int GetTime()
+        {
+            return time;
+        }
+
+        public virtual void SetTime(int time)
+        {
+            this.time = time;
+        }
+
+        public virtual string GetName()
+        {
+            return "";
+        }
+
+        public virtual void Initialize(EntityController entity)
+        {
+
+        }
+
+        public virtual void Tick(EntityController entity)
+        {
+            time -= 1;
+        }
+
+        public virtual void OnRemoved(EntityController entity)
+        {
+
+        }
+    }
+}
