c620a85231d701db59dc6dfc99d9140d3e782406
christides11 <29593332+christides11@users.noreply.github.com>
2020-09-21 14:15:38 -0700

    refactor: Added BoxDefinitionBase
    Start of letting BoxDefinition be able to be modified by the user.

---
+++ Assets/CAF/Combat/Boxes/BoxDefinitionBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CAF.Combat
+{
+    public class BoxDefinitionBase
+    {
+        public BoxShapes shape;
+
+        public BoxDefinitionBase()
+        {
+
+        }
+    }
+}
