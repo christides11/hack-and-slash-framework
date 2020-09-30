e4aaee6bcbb93f693ffab871777521c499934aad
christides11 <29593332+christides11@users.noreply.github.com>
2020-09-30 16:33:19 -0700

    feat: Added StateHurtboxDefinition.
    Start of support for defining the hurtboxes of an entity.

---
+++ Entities/Managers/EntityHurtboxManager.cs
@@ -0,0 +1,18 @@
+﻿using CAF.Input;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CAF.Entities
+{
+    public class EntityHurtboxManager : MonoBehaviour
+    {
+        [SerializeField] protected EntityManager manager;
+
+        public virtual void Tick()
+        {
+
+        }
+    }
+}
