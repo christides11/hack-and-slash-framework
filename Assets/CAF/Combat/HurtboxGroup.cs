36e3dffc02db9ba9d2da17e5c71b1c4ba0c76d8f
christides11 <29593332+christides11@users.noreply.github.com>
2020-09-30 16:33:19 -0700

    feat: Added StateHurtboxDefinition.
    Start of support for defining the hurtboxes of an entity.

---
+++ Assets/CAF/Combat/HurtboxGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CAF.Combat
+{
+    [System.Serializable]
+    public class HurtboxGroup
+    {
+        public int ID;
+        public int activeFramesStart = 1;
+        public int activeFramesEnd = 1;
+        [SerializeReference] public List<BoxDefinitionBase> boxes = new List<BoxDefinitionBase>();
+        public bool attachToEntity = true;
+        public string attachTo;
+
+        public HurtboxGroup()
+        {
+
+        }
+
+        public HurtboxGroup(HurtboxGroup other)
+        {
+
+        }
+    }
+}
---
+++ Assets/CAF/Combat/StateHurtboxDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CAF.Combat
+{
+    [CreateAssetMenu(fileName = "StateHurtbox", menuName = "CAF/StateHurtboxDefinition")]
+    public class StateHurtboxDefinition : ScriptableObject
+    {
+        public List<HurtboxGroup> hurtboxGroups = new List<HurtboxGroup>();
+    }
+}
