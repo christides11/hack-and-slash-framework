35033697db277781d415494cca400d9d3a3b1547
christides11 <29593332+christides11@users.noreply.github.com>
2021-05-07 01:22:11 -0700

    refactor: Changed to using PropertyDrawers and SerializedObjects for editors.
    BREAKING CHANGE: Now using propertydrawers and serializedobjects for editor. Makes undoing/redoing changes possible, and makes code for attack event editors able to be separate from the class itself.

---
+++ Editor/Combat/Attack/AttackEventDefinitionEditorWindow.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace CAF.Combat
+{
+    public class AttackEventDefinitionEditorWindow : EditorWindow
+    {
+        protected Dictionary<string, Type> attackEventTypes = new Dictionary<string, Type>();
+        public AttackDefinition attack;
+        public int eventIndex;
+
+        public static void Init(AttackDefinition attack, int eventIndex)
+        {
+            AttackEventDefinitionEditorWindow window =
+                (AttackEventDefinitionEditorWindow)EditorWindow.GetWindow(typeof(AttackEventDefinitionEditorWindow),
+                true,
+                attack.events[eventIndex].nickname
+                );
+            window.attack = attack;
+            window.eventIndex = eventIndex;
+            window.Show();
+        }
+
+        protected virtual void OnFocus()
+        {
+            attackEventTypes.Clear();
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var givenType in a.GetTypes())
+                {
+                    if (givenType.IsSubclassOf(typeof(AttackEvent)))
+                    {
+                        attackEventTypes.Add(givenType.FullName, givenType);
+                    }
+                }
+            }
+        }
+
+        Vector2 scrollPos;
+        protected virtual void OnGUI()
+        {
+            if (attack == null || attack.events.Count <= eventIndex)
+            {
+                Close();
+                return;
+            }
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+            DrawEventInfo();
+            EditorGUILayout.EndScrollView();
+        }
+
+        protected bool eventVariablesFoldout;
+        private void DrawEventInfo()
+        {
+            SerializedObject attackObject = new SerializedObject(attack);
+            attackObject.Update();
+
+            SerializedProperty eventProperty = attackObject.FindProperty("events").GetArrayElementAtIndex(eventIndex);
+
+            if(eventProperty == null)
+            {
+                return;
+            }
+
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("nickname"), new GUIContent("Nickname"));
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("active"), new GUIContent("Active"));
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("onHitCheck"), new GUIContent("On Hit Check"));
+            switch ((OnHitType)eventProperty.FindPropertyRelative("onHitCheck").enumValueIndex)
+            {
+                case OnHitType.ID_GROUP:
+                    EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("onHitIDGroup"), new GUIContent("ID Group"));
+                    break;
+                case OnHitType.HITBOX_GROUP:
+                    EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("onHitHitboxGroup"), new GUIContent("Hitbox Group"));
+                    break;
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Charge Required", GUILayout.Width(150));
+            EditorGUILayout.LabelField("min", GUILayout.Width(25));
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("chargeLevelMin"), GUIContent.none, GUILayout.Width(30));
+            EditorGUILayout.LabelField("max", GUILayout.Width(25));
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("chargeLevelMax"), GUIContent.none, GUILayout.Width(30));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("inputCheckTiming"));
+
+            EditorGUI.indentLevel++;
+            if ((AttackEventInputCheckTiming)eventProperty.FindPropertyRelative("inputCheckTiming").enumValueIndex != AttackEventInputCheckTiming.NONE)
+            {
+                EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("inputCheckStartFrame"));
+                EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("inputCheckEndFrame"));
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Input");
+                EditorGUILayout.PropertyField(eventProperty.FindPropertyRelative("input"));
+                EditorGUILayout.Space();
+            }
+            EditorGUI.indentLevel--;
+
+            EditorGUILayout.LabelField(attack.events[eventIndex].attackEvent == null ? "..." : attack.events[eventIndex].attackEvent.GetName());
+            var attackEventProperty = eventProperty.FindPropertyRelative("attackEvent");
+            if (GUILayout.Button("Set Event"))
+            {
+                GenericMenu menu = new GenericMenu();
+
+                foreach (string t in attackEventTypes.Keys)
+                {
+                    string destination = t.Replace('.', '/');
+                    menu.AddItem(new GUIContent(destination), true, OnAttackEventSelected, t);
+                }
+                menu.ShowAsContext();
+            }
+
+            eventVariablesFoldout = EditorGUILayout.Foldout(eventVariablesFoldout, "Variables", true);
+            if (eventVariablesFoldout)
+            {
+                EditorGUI.indentLevel++;
+                if (attack.events[eventIndex].attackEvent != null)
+                {
+                    EditorGUILayout.PropertyField(attackEventProperty);
+                }
+                EditorGUI.indentLevel--;
+            }
+
+            attackObject.ApplyModifiedProperties();
+        }
+
+        protected void OnAttackEventSelected(object t)
+        {
+            SerializedObject attackObject = new SerializedObject(attack);
+            attackObject.Update();
+            SerializedProperty eventProperty = attackObject.FindProperty("events").GetArrayElementAtIndex(eventIndex);
+            var attackEventProperty = eventProperty.FindPropertyRelative("attackEvent");
+            attackEventProperty.managedReferenceValue = Activator.CreateInstance(attackEventTypes[(string)t]);
+            attackObject.ApplyModifiedProperties();
+        }
+    }
+}
--- Editor/Combat/StateHurtboxDefinitionEditor.cs
+++ Editor/Combat/StateHurtboxDefinitionEditor.cs
@@ -8,8 +8,6 @@ namespace CAF.Combat
     [CustomEditor(typeof(StateHurtboxDefinition), true)]
     public class StateHurtboxDefinitionEditor : Editor
     {
-        StateHurtboxDefinition t;
-
         protected Dictionary<string, Type> hurtboxGroupTypes = new Dictionary<string, Type>();
 
         private void OnEnable()
@@ -29,6 +27,41 @@ namespace CAF.Combat
 
         public override void OnInspectorGUI()
         {
+            this.serializedObject.Update();
+
+            if(GUILayout.Button("Add Hurtbox Group"))
+            {
+                GenericMenu menu = new GenericMenu();
+
+                foreach (string hType in hurtboxGroupTypes.Keys)
+                {
+                    string destination = hType.Replace('.', '/');
+                    menu.AddItem(new GUIContent(destination), true, OnHurtboxGroupTypeSelected, hType);
+                }
+                menu.ShowAsContext();
+            }
+
+            SerializedProperty hurtboxGroupProperty = serializedObject.FindProperty("hurtboxGroups");
+
+            int lCount = hurtboxGroupProperty.arraySize;
+            for(int i = 0; i < lCount; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField($"Hurtbox Group {i}", GUILayout.MaxWidth(100));
+                if(GUILayout.Button("X", GUILayout.Width(30)))
+                {
+                    hurtboxGroupProperty.DeleteArrayElementAtIndex(i);
+                    break;
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(hurtboxGroupProperty.GetArrayElementAtIndex(i));
+                EditorGUI.indentLevel--;
+            }
+            serializedObject.ApplyModifiedProperties();
+
+            //this.DrawDefaultInspector();
+            /*
             t = target as StateHurtboxDefinition;
 
             if (GUILayout.Button("Add Hurtbox Group"))
@@ -56,18 +89,16 @@ namespace CAF.Combat
                 EditorGUI.indentLevel++;
                 t.hurtboxGroups[i].DrawInspector(15);
                 EditorGUI.indentLevel--;
-            }
-
-            if (GUI.changed)
-            {
-                EditorUtility.SetDirty(target);
-                GUI.changed = false;
-            }
+            }*/
         }
 
         private void OnHurtboxGroupTypeSelected(object type)
         {
-            t.hurtboxGroups.Add((HurtboxGroup)Activator.CreateInstance(hurtboxGroupTypes[(string)type]));
+            serializedObject.Update();
+            SerializedProperty property = serializedObject.FindProperty("hurtboxGroups");
+            property.InsertArrayElementAtIndex(property.arraySize);
+            property.GetArrayElementAtIndex(property.arraySize - 1).managedReferenceValue = Activator.CreateInstance(hurtboxGroupTypes[(string)type]);
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
