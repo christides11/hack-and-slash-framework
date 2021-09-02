048f49133fbe4318ab92b169dbd377d9c9a78279
christides11 <29593332+christides11@users.noreply.github.com>
2021-09-02 11:48:54 -0700

    feat: Removed unnecessary IFighterBase methods
    BREAKING CHANGE: Removed methods in IFighterBase that are not necessary.

--- Fighter/IFighterBase.cs
+++ Fighter/IFighterBase.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace HnSF.Fighters
 {
     public interface IFighterBase
     {
-        void Tick();
-        void LateTick();
-        void SetTargetable(bool value);
-        Vector3 GetMovementVector(float horizontal, float vertical);
-        Vector3 GetVisualBasedDirection(Vector3 direction);
-        void RotateVisual(Vector3 direction, float speed);
-        void SetVisualRotation(Vector3 direction);
         GameObject GetGameObject();
     }
 }
