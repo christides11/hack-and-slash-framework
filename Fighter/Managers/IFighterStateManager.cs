e46f8d85112e64349097b7459ca69db8cd4bde97
christides11 <29593332+christides11@users.noreply.github.com>
2021-09-02 11:30:04 -0700

    feat: Removed unnecessary IFighterStateManager methods and added RemoveState
    BREAKING CHANGE: Removed Tick and LateTick from IFighterStateManager, and added RemoveState.

--- Fighter/Managers/IFighterStateManager.cs
+++ Fighter/Managers/IFighterStateManager.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace HnSF.Fighters
+﻿namespace HnSF.Fighters
 {
     public interface IFighterStateManager
     {
         ushort CurrentState { get; }
         uint CurrentStateFrame { get; }
 
-        void Tick();
-        void LateTick();
         void AddState(FighterStateBase state, ushort stateNumber);
+        void RemoveState(ushort stateNumber);
         bool ChangeState(ushort state, uint stateFrame = 0, bool callOnInterrupt = true);
         FighterStateBase GetState(ushort state);
         void SetFrame(uint frame);
