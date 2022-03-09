48ce5f0fb545f38dfa19aee18d4165df404b0177
christides11 <29593332+christides11@users.noreply.github.com>
2022-03-08 19:39:02 -0500

    feat: TimelineClip passthrough.

--- Assets/HnSF/State/FighterStateAsset.cs
+++ Assets/HnSF/State/FighterStateAsset.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 namespace HnSF
 {
     public class FighterStateAsset : PlayableAsset
     {
+        [NonSerialized] public TimelineClip clipPassthrough = null;
+        
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
             throw new NotImplementedException($"{name} does not implement CreatePlayable");
--- Assets/HnSF/State/FighterStateBehaviour.cs
+++ Assets/HnSF/State/FighterStateBehaviour.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 namespace HnSF
 {
     [System.Serializable]
     public class FighterStateBehaviour : PlayableBehaviour
     {
+        [NonSerialized] public TimelineClip timelineClip;
+
         [SelectImplementation(typeof(StateConditionBase))] [SerializeField, SerializeReference]
         public StateConditionBase conditon = new StateConditionBoolean();
     }
