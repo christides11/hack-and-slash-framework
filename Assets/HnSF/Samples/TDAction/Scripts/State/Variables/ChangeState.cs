4e30a305522b105bdd977c348a457ab1407991db
christides11 <29593332+christides11@users.noreply.github.com>
2022-07-05 23:34:44 -0700

    refactor: flattened statetimeline variable list
    BREAKING CHANGE: IStateVariables now refer to their children by ID.

--- Assets/HnSF/Samples/TDAction/Scripts/State/Variables/ChangeState.cs
+++ Assets/HnSF/Samples/TDAction/Scripts/State/Variables/ChangeState.cs
@@ -4,22 +4,36 @@ namespace HnSF.Sample.TDAction.State
 {
     public struct ChangeState : IStateVariables
     {
+        public string name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int ID
+        {
+            get => id;
+            set => id = value;
+        }
         public int FunctionMap => (int)StateFunctionEnum.CHANGE_STATE;
         public IConditionVariables Condition => condition;
-        public IStateVariables[] Children => children;
+        public int Parent => Parent;
+        public int[] Children => children;
         public Vector2[] FrameRanges
         {
             get => frameRanges;
             set => frameRanges = value;
         }
-    
+   
+        public int id;
         [SerializeField] public Vector2[] frameRanges;
         [SelectImplementation(typeof(IConditionVariables))] [SerializeField, SerializeReference] 
         public IConditionVariables condition;
 
         public int stateMovesetID;
         public int stateID;
-        [SelectImplementation(typeof(IConditionVariables))] [SerializeField, SerializeReference] 
-        private IStateVariables[] children;
+        
+        public int parent;
+        public int[] children;
     }
 }
--- Assets/HnSF/Samples/TDAction/Scripts/State/Variables/VarSetFallSpeed.cs
+++ Assets/HnSF/Samples/TDAction/Scripts/State/Variables/VarSetFallSpeed.cs
@@ -4,9 +4,21 @@ namespace HnSF.Sample.TDAction.State
 {
     public struct VarSetFallSpeed : IStateVariables
     {
+        public string name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int ID
+        {
+            get => id;
+            set => id = value;
+        }
         public int FunctionMap => (int)StateFunctionEnum.SET_FALL_SPEED;
         public IConditionVariables Condition => condition;
-        public IStateVariables[] Children => children;
+        public int Parent => Parent;
+        public int[] Children => children;
 
         public Vector2[] FrameRanges
         {
@@ -14,13 +26,14 @@ namespace HnSF.Sample.TDAction.State
             set => frameRanges = value;
         }
     
+        public int id;
         [SerializeField] public Vector2[] frameRanges;
         [SelectImplementation(typeof(IConditionVariables))] [SerializeField, SerializeReference] 
         public IConditionVariables condition;
 
         public float value;
         
-        [SelectImplementation(typeof(IStateVariables))] [SerializeField, SerializeReference] 
-        private IStateVariables[] children;
+        public int parent;
+        public int[] children;
     }
 }
