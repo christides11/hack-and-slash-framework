8ad3c5f4123f68e20ad68f55ada440f534113793
Chris Collins <29593332+christides11@users.noreply.github.com>
2022-07-23 18:02:32 -0700

    feat: Name for IntStateMap

--- State/IntStateMap.cs
+++ State/IntStateMap.cs
@@ -5,6 +5,7 @@ namespace HnSF
     [System.Serializable]
     public class IntStateMap
     {
+        public string name;
         [SelectImplementation((typeof(FighterStateReferenceBase)))] [SerializeField, SerializeReference]
         public FighterStateReferenceBase state = new FighterStateReferenceBase();
         public StateTimeline stateTimeline;
