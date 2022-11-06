1000da899b8edae7e0d3097ec49dad4fc4244b03
Chris Collins <29593332+christides11@users.noreply.github.com>
2022-11-06 15:57:27 -0800

    refactor: MovesetDefinition to IMovesetDefinition
    BREAKING CHANGE

--- Assets/HnSF/Samples/TDAction/Scripts/Fighter/MovesetDefinition.cs
+++ Assets/HnSF/Samples/TDAction/Scripts/Fighter/MovesetDefinition.cs
@@ -1,20 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using HnSF.Combat;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace HnSF.Sample.TDAction
 {
     [CreateAssetMenu(menuName = "2dAction/Moveset")]
-    public class MovesetDefinition : Combat.MovesetDefinition
+    public class MovesetDefinition : ScriptableObject, IMovesetDefinition
     {
-        public override void Initialize()
+        [NonSerialized] protected Dictionary<int, HnSF.StateTimeline> stateMap;
+        [FormerlySerializedAs("states")]
+        [Header("States")] 
+        [SerializeField] protected IntStateMap[] groundActions = Array.Empty<IntStateMap>();
+        [SerializeField] protected IntStateMap[] airActions = Array.Empty<IntStateMap>();
+        [SerializeField] protected IntStateMap[] groundAttacks = Array.Empty<IntStateMap>();
+        [SerializeField] protected IntStateMap[] airAttacks = Array.Empty<IntStateMap>();
+        
+        public virtual void Initialize()
         {
-            base.Initialize();
+            if (stateMap == null) stateMap = new Dictionary<int, HnSF.StateTimeline>();
+            if (stateMap.Count ==
+                groundActions.Length + airActions.Length + groundAttacks.Length + airAttacks.Length) return;
+            stateMap.Clear();
 
-            foreach (var stateMap in states)
+            foreach (var action in groundActions)
             {
-                stateMap.stateTimeline.Initialize();
+                stateMap.Add(action.state.GetState(), action.stateTimeline);
             }
+            
+            foreach (var action in airActions)
+            {
+                stateMap.Add(action.state.GetState(), action.stateTimeline);
+            }
+            
+            foreach (var action in groundAttacks)
+            {
+                stateMap.Add(action.state.GetState(), action.stateTimeline);
+            }
+            
+            foreach (var action in airAttacks)
+            {
+                stateMap.Add(action.state.GetState(), action.stateTimeline);
+            }
+        }
+
+        public HnSF.StateTimeline GetState(int stateID)
+        {
+            return stateMap[stateID];
         }
     }
 }
