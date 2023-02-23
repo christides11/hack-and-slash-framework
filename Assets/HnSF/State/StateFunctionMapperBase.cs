ffd4229e88dfae1b47019cc9d976990d3d04bff6
Chris Collins <29593332+christides11@users.noreply.github.com>
2023-02-23 12:58:21 -0800

    feat: State frame range % for functions
    BREAKING CHANGE: Added frame range percentage for functions.

--- Assets/HnSF/State/StateFunctionMapperBase.cs
+++ Assets/HnSF/State/StateFunctionMapperBase.cs
@@ -7,22 +7,25 @@ namespace HnSF
     [System.Serializable]
     public class StateFunctionMapperBase
     {
-        public Dictionary<Type, Action<IFighterBase, IStateVariables, StateTimeline, int>> functions =
-            new Dictionary<Type, Action<IFighterBase, IStateVariables, StateTimeline, int>>();
+        /// <summary>
+        /// IFighterBase, IStateVariables, StateTimeline, Current Frame, Frame Range %
+        /// </summary>
+        public Dictionary<Type, Action<IFighterBase, IStateVariables, StateTimeline, int, float>> functions =
+            new Dictionary<Type, Action<IFighterBase, IStateVariables, StateTimeline, int, float>>();
 
-        public virtual bool TryRegisterFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int> function)
+        public virtual bool TryRegisterFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int, float> function)
         {
             if (functions.ContainsKey(id)) return false;
             functions.Add(id, function);
             return true;
         }
 
-        public virtual void RegisterFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int> function)
+        public virtual void RegisterFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int, float> function)
         {
             functions.Add(id, function);
         }
 
-        public virtual bool OverrideFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int> function)
+        public virtual bool OverrideFunction(Type id, Action<IFighterBase, IStateVariables, StateTimeline, int, float> function)
         {
             functions[id] = function;
             return true;
